balabit-deps/syslog-ng-6.0-core

commit 9034bdf00341821fcdb896d13d145caafaf635c0

Reload: avoid use after free of the main_loop_old_config
Previously the uniqid of the main_loop_old_config was checked after the struct is freed
Caused use after free, and possibly crush dump

Signed-off-by: Juhász Viktor <viktor.juhasz@balabit.com>

lib/mainloop.c
@@ -347,12 +347,6 @@ main_loop_reload_config_apply(void)
         {
           main_loop_new_config->stats_reset = TRUE;
         }
-      cfg_free(main_loop_old_config);
-      persist_config_free(main_loop_new_config->persist);
-      main_loop_new_config->persist = NULL;
-      current_configuration = main_loop_new_config;
-      service_management_clear_status();
-
       if (main_loop_old_config->use_uniqid && !current_configuration->use_uniqid)
         {
           log_msg_deinit_rcptid();
@@ -361,6 +355,11 @@ main_loop_reload_config_apply(void)
         {
           log_msg_init_rcptid(current_configuration->state);
         }
+      cfg_free(main_loop_old_config);
+      persist_config_free(main_loop_new_config->persist);
+      main_loop_new_config->persist = NULL;
+      current_configuration = main_loop_new_config;
+      service_management_clear_status();
     }
   else
     {

commit 67b0c1d33af2607c99699ca2a00657c5d00e4992

mainloop: fix use_uniqid check on reload
This is a regression caused by 9034bdf00341821fcdb896d13d145caafaf635c0,
where I suggested to move the current_configuration assignment to the end
of the main_loop_reload_config_apply() function.

We checked the use_uniqid field of current_configuration
instead of main_loop_new_config->use_uniqid.

Fixes #SYSLOGDEV-2358

Signed-off-by: László Várady <laszlo.varady@balabit.com>

lib/mainloop.c
@@ -347,14 +347,15 @@ main_loop_reload_config_apply(void)
         {
           main_loop_new_config->stats_reset = TRUE;
         }
-      if (main_loop_old_config->use_uniqid && !current_configuration->use_uniqid)
+      if (main_loop_old_config->use_uniqid && !main_loop_new_config->use_uniqid)
         {
           log_msg_deinit_rcptid();
         }
-      else if (!main_loop_old_config->use_uniqid && current_configuration->use_uniqid)
+      else if (!main_loop_old_config->use_uniqid && main_loop_new_config->use_uniqid)
         {
-          log_msg_init_rcptid(current_configuration->state);
+          log_msg_init_rcptid(main_loop_new_config->state);
         }
+
       cfg_free(main_loop_old_config);
       persist_config_free(main_loop_new_config->persist);
       main_loop_new_config->persist = NULL;